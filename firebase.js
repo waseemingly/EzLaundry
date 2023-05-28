b78419bcde19543f5b28ccb89a8e37a8fbc15e90
waseemingly <mohammedwaseem1000@gmail.com>
2023-05-28 16:16:40 +0800

    Co-authored-by: pradeep-cod <pradeep-cod@users.noreply.github.com>

---
+++ firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase/compat";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIP1nL3YbxXsABT5foyaaVoMOMXizsdZo",
+  authDomain: "ezlaundry-9ec2a.firebaseapp.com",
+  projectId: "ezlaundry-9ec2a",
+  storageBucket: "ezlaundry-9ec2a.appspot.com",
+  messagingSenderId: "969724570074",
+  appId: "1:969724570074:web:d0abce76a5e88dee2c78e3",
+  measurementId: "G-J6JMJ0X4Z9"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+// initialize auth
+const auth = firebase.auth()
+
+export { auth };
---
+++ screens/HomeScreen.js
@@ -0,0 +1,123 @@
+import { useNavigation } from '@react-navigation/core'
+import React from 'react'
+import { Button, useWindowDimensions, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import { auth } from '../firebase'
+import booking from '../assets/booking.png';
+import authenticate from '../assets/authenticate.png';
+import cancel from '../assets/Cancel.png';
+import penalties from '../assets/penalties.png';
+import signout from '../assets/signout.png';
+
+
+
+const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login")
+      })
+      .catch(error => alert(error.message))
+  }
+
+  const {height} = useWindowDimensions()
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+      style={styles.button}>
+        <Image
+        style={{width: 90, height: 40}}
+        source={booking}
+        resizeMode="cover"
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}>
+        <Image
+        style={{width: 90, height: 40}}
+        source={authenticate}
+        resizeMode="cover"
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}>
+        <Image
+        style={{width: 90, height: 40}}
+        source={cancel}
+        resizeMode="cover"
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}>
+        <Image
+        style={{width: 90, height: 40}}
+        source={penalties}
+        resizeMode="cover"
+        />
+      </TouchableOpacity>
+
+      {/*
+      <Text>Email: {auth.currentUser?.email}</Text>
+      */}
+
+      <TouchableOpacity
+        onPress={handleSignOut}
+        style={styles.button}>
+        <Image
+        style={{width: 90, height: 40}}
+        source={signout}
+        resizeMode="cover"
+        /> 
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
+
+      
+  
+      
+    </View>
+
+  )
+}  
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+   button: {
+    backgroundColor: 'white',
+    width: '30%',
+    height: '10%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
+
+
+{/*
+
+     <View style={styles.root}>
+        <Image
+          source={Logo}
+          style={[styles.logo, {height: height * 0.3}]}
+          resizeMode="contain"
+        />
+      </View>
+*/}
+
