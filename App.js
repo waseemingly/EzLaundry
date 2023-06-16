e2022d130ec4fa14e89392e76719e55fa792d8ab
pradeep-cod <pradeepmanimaran@gmail.com>
2023-06-17 01:24:28 +0800

    forgot password

--- App.js
+++ App.js
@@ -6,12 +6,29 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import RegisterScreen from './screens/RegisterScreen';
-import ForgotPasswordScreen from './ForgotPasswordScreen';
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
 import firebase from 'firebase';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // Your Firebase configuration
+  apiKey: "AIzaSyBIP1nL3YbxXsABT5foyaaVoMOMXizsdZo",
+  authDomain: "ezlaundry-9ec2a.firebaseapp.com",
+  projectId: "ezlaundry-9ec2a",
+  storageBucket: "ezlaundry-9ec2a.appspot.com",
+  messagingSenderId: "969724570074",
+  appId: "1:969724570074:web:d0abce76a5e88dee2c78e3",
+  measurementId: "G-J6JMJ0X4Z9"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
