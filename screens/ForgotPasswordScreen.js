43d963144e4ff6a9775be5f107ad1dc3f9a0ec08
pradeep-cod <pradeepmanimaran@gmail.com>
2023-06-17 23:43:39 +0800

    delete

--- screens/ForgotPasswordScreen.js
+++ screens/ForgotPasswordScreen.js
@@ -8,25 +8,6 @@ import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBIP1nL3YbxXsABT5foyaaVoMOMXizsdZo",
-    authDomain: "ezlaundry-9ec2a.firebaseapp.com",
-    projectId: "ezlaundry-9ec2a",
-    storageBucket: "ezlaundry-9ec2a.appspot.com",
-    messagingSenderId: "969724570074",
-    appId: "1:969724570074:web:d0abce76a5e88dee2c78e3",
-    measurementId: "G-J6JMJ0X4Z9"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
- // const analytics = getAnalytics(app); track analyse user behavior
-  
-  
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-
 const ForgotPasswordScreen = () => {
     const [email, setEmail] = useState('');
 
